77a95365ebc12e239f571c5b87c127230a7a4dee
Jackie <jackie>
2020-07-16 17:53:19 +0800

    no message

--- SwiftNotification/PushViewController.swift
+++ SwiftNotification/PushViewController.swift
@@ -12,22 +12,56 @@ class PushViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.notifiCenter.addNotification("123", callBack: { (noti) in
+        self.notifiCenter.addNotification("456", callBack: { (noti) in
             print("\(String(describing: noti.object)) -- \(String(describing: noti.userInfo))")
+        })
+        
+        self.notifiCenter.addNotification("456", callBack: { (noti) in
+            print("\(String(describing: noti.object)) -- \(String(describing: noti.userInfo))")
+        })
+        
+        self.notifiCenter.addNotification("456", callBack: { (noti) in
+            print("\(String(describing: noti.object)) -- \(String(describing: noti.userInfo))")
+        })
+        
+        self.notifiCenter.addNotification(UIApplication.willEnterForegroundNotification, callBack: { (noti) in
+            print("1111111-- \(String(describing: noti.userInfo))")
+        })
+        self.notifiCenter.addNotification(UIApplication.willEnterForegroundNotification, callBack: { (noti) in
+            print("1111111-- \(String(describing: noti.userInfo))")
+        })
+        self.notifiCenter.addNotification(UIApplication.willEnterForegroundNotification, callBack: { (noti) in
+            print("1111111-- \(String(describing: noti.userInfo))")
         })
         // Do any additional setup after loading the view.
         let btn = UIButton.init(type: .custom)
         btn.frame = CGRect.init(x: 100, y: 100, width: 200, height: 100)
-        btn.setTitle("发通知", for: .normal)
+        btn.setTitle("删通知", for: .normal)
         btn.setTitleColor(.black, for: .normal)
         btn.backgroundColor = .yellow
-        btn.addTarget(self, action: #selector(push), for: .touchUpInside)
+        btn.addTarget(self, action: #selector(deleteNoti), for: .touchUpInside)
         self.view.addSubview(btn)
+        
+        let pushbtn = UIButton.init(type: .custom)
+        pushbtn.frame = CGRect.init(x: 100, y: 200, width: 200, height: 100)
+        pushbtn.setTitle("发通知", for: .normal)
+        pushbtn.setTitleColor(.black, for: .normal)
+        pushbtn.backgroundColor = .yellow
+        pushbtn.addTarget(self, action: #selector(push), for: .touchUpInside)
+        self.view.addSubview(pushbtn)
     }
     
         @objc func push(){
-            self.notifiCenter.postNotification("123", object: "aaaaaaa", userInfo: ["key":"fefe"])
+            print("发通知")
+            self.notifiCenter.postNotification("456", object: "aaaaaaa", userInfo: ["key":"fefe"])
         }
+    
+    @objc func deleteNoti(){
+//            self.notifiCenter.postNotification("123", object: "aaaaaaa", userInfo: ["key":"fefe"])
+        print("删通知")
+        self.notifiCenter.removeNotification(name : "456")
+        self.notifiCenter.removeNotification(notiName: UIApplication.willEnterForegroundNotification)
+    }
     /*
     // MARK: - Navigation
 
--- SwiftNotification/SwiftNotification.swift
+++ SwiftNotification/SwiftNotification.swift
@@ -34,43 +34,68 @@ public extension NSObject {
  
 public class NotificationTool: NSObject {
     
-    var observers = [String : Any]()
+    var observers = [NSNotification.Name : Any]()
     
     deinit {
-        removeAllObserver()
+        removeAllNotification()
     }
     
     /// 移除所有通知
-    public func removeAllObserver(){
+    public func removeAllNotification(){
         observers.keys.forEach { (name) in
-            removeObserverWithName(name)
+            removeNotification(name: name.rawValue)
         }
     }
     
     /// 移除单个通知
-    /// - Parameter name: 通知名称
-    public func removeObserverWithName(_ name : String?){
-        guard let na = name, let ob = observers[na] else { return }
+    /// - Parameter name: 通知名称String
+    public func removeNotification(name : String){
+        guard let ob = observers[NSNotification.Name.init(rawValue: name)] else { return }
         NotificationCenter.default.removeObserver(ob)
+        observers.removeValue(forKey: NSNotification.Name.init(rawValue: name))
+    }
+    
+    /// 移除单个系统通知
+    /// - Parameter forName: NSNotification.Name通知名称
+    public func removeNotification(notiName : NSNotification.Name){
+        removeNotification(name: notiName.rawValue)
     }
     
     /// 移除多个通知
-    /// - Parameter names: 通知名称数组
-    public func removeObserverWithNames(_ names : [String]){
+    /// - Parameter names: 通知名称String数组
+    public func removeNotifications(names : [String]){
         names.forEach { (name) in
-            removeObserverWithName(name)
+            removeNotification(name: name)
+        }
+    }
+    
+    /// 移除多个系统通知
+    /// - Parameter notiNames: 通知名称NSNotification.Name数组
+    public func removeNotifications(notiNames : [NSNotification.Name]){
+        notiNames.forEach { (name) in
+            removeNotification(notiName: name)
         }
     }
     
     /// 注册添加通知
     /// - Parameters:
-    ///   - name: 通知名称
+    ///   - name: 通知名称String
     ///   - callBack: 通知执行的回调
     public func addNotification(_ name : String, callBack : @escaping (Notification) -> Void){
+        removeNotification(name: name)
         let ob = NotificationCenter.default.addObserver(forName: NSNotification.Name.init(rawValue: name), object: nil, queue: OperationQueue.main, using: callBack)
-        observers.updateValue(ob, forKey: name)
+        observers.updateValue(ob, forKey: NSNotification.Name.init(rawValue: name))
     }
     
+    /// 注册添加系统通知
+    /// - Parameters:
+    ///   - notiName: NSNotification.Name通知名称
+    ///   - callBack: 通知执行的回调
+    public func addNotification(_ notiName : NSNotification.Name, callBack : @escaping (Notification) -> Void){
+        removeNotification(notiName: notiName)
+        let ob = NotificationCenter.default.addObserver(forName: notiName, object: nil, queue: OperationQueue.main, using: callBack)
+        observers.updateValue(ob, forKey: notiName)
+    }
     
     /// 发送通知
     /// - Parameters:
